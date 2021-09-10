6c9583ae183c3de9abbf0191dfc856d46c82c28e
sws <1275659412@qq.com>
2021-09-10 17:48:37 +0800

    target pointer

--- MyCode/内存管理01/内存管理01/ViewController.m
+++ MyCode/内存管理01/内存管理01/ViewController.m
@@ -9,6 +9,8 @@
 #import "SecViewController.h"
 @interface ViewController ()
 
+@property (nonatomic, strong) NSString *name;
+
 @end
 
 @implementation ViewController
@@ -16,8 +18,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor lightGrayColor];
+    [self testForSetName];
 }
 
+
+- (void)testForSetName{
+    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+    /*
+        崩溃，EXC_BAD_ACCESS (code=257, address=0x66656463626111)
+        原因，多次release name
+        解决方案： 1、加锁，解锁 2、减小 string 长度 3、name 属性加锁。
+     */
+//    for (int i = 0; i < 1000; ++i) {
+//        dispatch_async(queue, ^{
+//            // 加锁
+//            self.name = [NSString stringWithFormat:@"abcdefghijklmnopq"];
+//            // 解锁
+//        });
+//    }
+    /**
+        不崩溃，原因，多次release name
+        解决方案： 1、加锁，解锁 2、减小 string 长度
+     */
+    for (int i = 0; i < 1000; ++i) {
+        dispatch_async(queue, ^{
+            self.name = [NSString stringWithFormat:@"abc"];
+        });
+    }
+    
+    NSString *str1 = [NSString stringWithFormat:@"abc"];
+    NSString *str2 = [NSString stringWithFormat:@"abcdefghijklmnopq"];
+    NSLog(@"str1 = %p, str2 = %p", str1, str2);
+}
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     [self.navigationController pushViewController:[[SecViewController alloc] init] animated:YES];
--- MyCode/内存管理01/内存管理01/main.m
+++ MyCode/内存管理01/内存管理01/main.m
@@ -10,21 +10,46 @@
 #import "WSProxy.h"
 #import "WSProxy1.h"
 #import "ViewController.h"
+
+BOOL isTargetPointer(id pointer){
+    return (long)(__bridge void *)pointer & 1;
+}
+
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
-        ViewController *vc = [[ViewController alloc] init];
-        WSProxy *proxy = [WSProxy proxyWithTarget:vc];
-        WSProxy1 *proxy1 = [WSProxy1 proxyWithTarget:vc];
+//        ViewController *vc = [[ViewController alloc] init];
+//        WSProxy *proxy = [WSProxy proxyWithTarget:vc];
+//        WSProxy1 *proxy1 = [WSProxy1 proxyWithTarget:vc];
+//
+//        NSLog(@"%d", [proxy isKindOfClass:[ViewController class]]);// 1
+//        NSLog(@"%d", [proxy1 isKindOfClass:[ViewController class]]);// 左边对象是否右边对象类型，或者右边类型的子类 // 0
+//
+//        NSString *str1 = @"123";
+//        NSString *str2 = @"123";
+//        NSString *str3 = @"456";
+//        NSLog(@"%p , %p, %p", str1, str2, str3);
+#pragma mark
+#pragma mark targetPoint
+#pragma mark
         
-        NSLog(@"%d", [proxy isKindOfClass:[ViewController class]]);// 1
-        NSLog(@"%d", [proxy1 isKindOfClass:[ViewController class]]);// 左边对象是否右边对象类型，或者右边类型的子类 // 0
+        /**
+         一些小对象的存储 tagged pointer
+         */
+        NSNumber *number = @4;
+        NSNumber *number1 = @5;
+        NSNumber *number2 = @6;
         
-        NSString *str1 = @"123";
-        NSString *str2 = @"123";
-        NSString *str3 = @"456";
-        NSLog(@"%p , %p, %p", str1, str2, str3);
+        NSLog(@"%p,%p, %p", number, number1, number2);
+        NSLog(@"%d %d, %d", isTargetPointer(number), isTargetPointer(number1), isTargetPointer(number2));
+        NSLog(@"%@ %@, %@", [number class], [number1 class], [number2 class]);
         
+        NSString *str = [NSString stringWithFormat:@"123"];
+        NSString *str1 = [NSString stringWithFormat:@"aefasfasdfasfdasf"];
+        NSLog(@"%p %p", str, str1);
+        NSLog(@"%@  %@",[str class],[str1 class]);
+
+
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
